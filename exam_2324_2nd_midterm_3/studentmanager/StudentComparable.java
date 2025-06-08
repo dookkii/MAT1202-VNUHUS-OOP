e25ff0ce0702a621bfd68cf4934fd972cca0afe1
Nguyễn Đức Tuân <65776978+dookkii@users.noreply.github.com>
2025-06-08 20:24:59 +0700

    Delete due to sensitive reasons.

--- exam_2324_2nd_midterm_3/studentmanager/StudentComparable.java
+++
@@ -1,5 +0,0 @@
-package exam_2324_2nd_midterm_3.studentmanager;
-
-public interface StudentComparable extends Comparable<Student> {
-    int compareTo(Student another);
-}
--- exam_2324_2nd_midterm_3/studentmanager/StudentComparator.java
+++
@@ -1,7 +0,0 @@
-package exam_2324_2nd_midterm_3.studentmanager;
-
-import java.util.Comparator;
-
-public interface StudentComparator extends Comparator<Student> {
-    int compare(Student left, Student right);
-}
