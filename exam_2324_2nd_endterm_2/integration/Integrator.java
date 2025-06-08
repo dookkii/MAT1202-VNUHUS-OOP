5da24be7e2244eee5813296c56533d13a776289d
Nguyễn Đức Tuân <65776978+dookkii@users.noreply.github.com>
2025-06-08 20:24:41 +0700

    Delete due to sensitive reasons.

--- exam_2324_2nd_endterm_2/integration/Integrator.java
+++
@@ -1,5 +0,0 @@
-package exam_2324_2nd_endterm_2.integration;
-
-public interface Integrator {
-    double integrate(Polynomial poly, double lower, double upper);
-}
--- exam_2324_2nd_endterm_2/mystudentmanager/MyIterable.java
+++
@@ -1,5 +0,0 @@
-package exam_2324_2nd_endterm_2.mystudentmanager;
-
-public interface MyIterable {
-    MyIterator iterator();
-}
